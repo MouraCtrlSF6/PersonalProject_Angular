35c3e44e20f71baaa73e955bb9cc49bbacfdf487
lucas.moura <lucas.moura@meta.com.br>
2022-09-08 14:30:14 -0300

    feat: Project rename

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProjetoPessoal'`, () => {
+  it(`should have as title 'GerenciamentoDeUsuarios'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProjetoPessoal');
+    expect(app.title).toEqual('GerenciamentoDeUsuarios');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProjetoPessoal app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('GerenciamentoDeUsuarios app is running!');
   });
 });
